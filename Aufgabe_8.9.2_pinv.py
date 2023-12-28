b22d55b158fca46c0e502bb394a06fc1432e765b
LGoellmann <138715461+LGoellmann@users.noreply.github.com>
2023-12-28 17:58:03 +0100

    Add files via upload

--- Aufgabe_8.9.2_pinv.py
+++ Aufgabe_8.9.2_pinv.py
@@ -7,7 +7,21 @@ print(A)
 
 np.set_printoptions(formatter={'float':"{0:8.4f}".format})
 
-print('\npinv=')
-print(np.linalg.pinv(A))
+P = np.linalg.pinv(A)
+print('\npinv(A)=')
+print(P)
+
+b1 = np.array([5,3,0,0,0])
+b2 = np.array([5,3,8,6,5])
+
+print('\npinv(A).b1')
+print(P@b1)
+
+print('\npinv(A).b2')
+print(P@b2)
+
+print('\nMinimalabstand von b1 zu Bild(A):')
+print(np.linalg.norm(A@P@b1-b1,2))
+
 
 
